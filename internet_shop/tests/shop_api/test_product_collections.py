b3320b131075e112ca2d1705a56984e175700b27
andrey <andrey_belyanin@mail.ru>
2021-02-18 23:51:35 +1000

    добавлен тест обновления о подборке админом

--- internet_shop/tests/shop_api/test_product_collections.py
+++ internet_shop/tests/shop_api/test_product_collections.py
@@ -6,7 +6,7 @@ from shop_api.models import ProductCollections, Product
 
 def test_create_product_collections_by_admin_and_list_by_user(admin_client, client):
     """ Тест создания подборок админом и вывода списка подборок пользователем """
-    """ Создание подборок товаров админом"""
+    """ Создание товаров админом"""
     url = reverse("products-list")
     product = {"name": "компьютер", "price": 10000, "description": "для офиса"}
     product1 = {"name": "принтер", "price": 5000, "description": "для офиса"}
@@ -20,6 +20,7 @@ def test_create_product_collections_by_admin_and_list_by_user(admin_client, clie
     assert response1.status_code == HTTP_201_CREATED
     assert response2.status_code == HTTP_201_CREATED
     assert response3.status_code == HTTP_201_CREATED
+    """ Создание подборок админом """
     url = reverse("product-collections-list")
     collection = {"title": "для работы", "text": "надёжная техника для работы"}
     collection1 = {"title": "для развлечений", "text": "техника для развлечений"}
@@ -27,6 +28,7 @@ def test_create_product_collections_by_admin_and_list_by_user(admin_client, clie
     response1 = admin_client.post(url, collection1)
     assert response.status_code == HTTP_201_CREATED
     assert response1.status_code == HTTP_201_CREATED
+    """ Добавление товаров в подборки админом """
     collection_info = ProductCollections.objects.get(title=collection['title'])
     collection_info1 = ProductCollections.objects.get(title=collection1['title'])
     product_info = Product.objects.get(name=product['name'])
@@ -46,6 +48,14 @@ def test_create_product_collections_by_admin_and_list_by_user(admin_client, clie
     assert response1.status_code == HTTP_201_CREATED
     assert response2.status_code == HTTP_201_CREATED
     assert response3.status_code == HTTP_201_CREATED
+    """ Изменение информации о подборке админом """
+    new_text = {'title': 'для дома и развлечений', 'text': 'отличная техника для дома и развлечений'}
+    collection_info = ProductCollections.objects.get(title='для развлечений')
+    url = reverse("product-collections-detail", args=(collection_info.id,))
+    resp = admin_client.put(url, data=new_text, content_type='application/json')
+    new_collection = ProductCollections.objects.get(title=new_text['title'])
+    assert resp.status_code == HTTP_200_OK
+    assert new_collection.text == "отличная техника для дома и развлечений"
     """ Получение списка подборок пользователем """
     url = reverse("product-collections-list")
     resp = client.get(url)
