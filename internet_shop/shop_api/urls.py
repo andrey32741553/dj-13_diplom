066434529cd1c3d4b697f8132f32c60545b37a16
andrey <andrey_belyanin@mail.ru>
2021-02-20 18:10:18 +1000

    добавлен путь для избранных товаров

--- internet_shop/shop_api/urls.py
+++ internet_shop/shop_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from shop_api.views import ProductViewSet, ReviewViewSet, OrderViewSet, PositionViewSet, CollectionViewSet, \
-    AddProductToCollectionViewSet
+    AddProductToCollectionViewSet, FavouritesViewSet, UserViewSet
 
 urlpatterns = format_suffix_patterns([
     path('products/', ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
@@ -10,9 +10,13 @@ urlpatterns = format_suffix_patterns([
     path('product-reviews/', ReviewViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('product-reviews/<int:pk>/', ReviewViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('orders/', OrderViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('orders/<int:pk>/', OrderViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
+    path('orders/<int:pk>/', OrderViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('positions/', PositionViewSet.as_view({'post': 'create'})),
     path('product-collections/', CollectionViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('product-collections/<int:pk>/', CollectionViewSet.as_view({'get': 'retrieve'})),
-    path('product-to-collection/', AddProductToCollectionViewSet.as_view({'post': 'create'}))
+    path('product-collections/<int:pk>/', CollectionViewSet.as_view({'get': 'retrieve', 'put': 'update',
+                                                                     'delete': 'destroy'})),
+    path('product-add-to-collection/', AddProductToCollectionViewSet.as_view({'post': 'create'})),
+    path('favourites/', FavouritesViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('user-info/', UserViewSet.as_view({'get': 'list'})),
+    path('user-info/<int:pk>/', UserViewSet.as_view({'get': 'retrieve'}))
 ])
