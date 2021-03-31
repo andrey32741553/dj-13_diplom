36d38c294b0cc9f41ce1ab8f519139a9609a3439
andrey32741553 <fylhtq32741553>
2021-03-31 23:06:59 +1000

    djoser deleted

--- internet_shop/internet_shop/urls.py
+++ internet_shop/internet_shop/urls.py
@@ -30,6 +30,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('shop_api.urls')),
     path('api/v1/', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
 ]
